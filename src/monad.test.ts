c5089f125e6c948a8dfc89b6d91616b293c99232
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-01 08:45:49 +0200

    monad example using flatMap

--- src/monad.test.ts
+++ src/monad.test.ts
@@ -4,3 +4,9 @@ test("compose lifting functions", () => {
   const twiceTheIncrement = composeLifting(liftingDouble, liftingInc);
   expect(twiceTheIncrement(10)).toStrictEqual([22]);
 });
+
+test("flat map", () => {
+  const numbers = [1, 2, 3];
+  const incrementDoubled = composeLifting(liftingInc, liftingDouble);
+  expect(numbers.flatMap(incrementDoubled)).toStrictEqual([3, 5, 7]);
+});
